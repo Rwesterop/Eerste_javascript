48e168f32f100b11ae71835304ea9e11afc26170
Rwesterop <71023911+Rwesterop@users.noreply.github.com>
2021-07-06 15:22:18 +0200

    Add files via upload

---
+++ Html/Javascript/Script.js
@@ -0,0 +1,15 @@
+
+        console.log("Hello Winc Academy ");
+        let name = 'Rick';
+        console.log("name");
+        
+
+        console.log(4+4);
+        console.log(3-2);
+        console.log(3*2);
+        console.log(3/2);
+        console.log(50 % 20);
+        let age = 'twentyseven';
+        console.log(age);
+        console.log(typeof age);
+
